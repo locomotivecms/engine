7d30d00ec4c35b0560409401072481fe9d19d455
Dirk Kelly <dk@dirkkelly.com>
2011-02-01 17:05:36 +0800

    minimal attributes to include listed

--- app/models/page.rb
+++ app/models/page.rb
@@ -43,7 +43,7 @@ class Page
   scope :not_found, :where => { :slug => '404', :depth => 0 }
   scope :published, :where => { :published => true }
   scope :fullpath, lambda { |fullpath| { :where => { :fullpath => fullpath } } }
-  scope :minimal_attributes, :only => %w(title slug fullpath position depth published templatized parent_id created_at updated_at)
+  scope :minimal_attributes, :only => %w(title slug fullpath position depth published templatized listed parent_id created_at updated_at)
 
   ## methods ##
 
