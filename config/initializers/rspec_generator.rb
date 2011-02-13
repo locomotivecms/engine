1aee2f4ce5ea34af8677b8399f45ae2ee0c6e1ea
did <didier@nocoffee.fr>
2011-02-13 01:12:27 +0100

    it prevented the use of rspec when locomotive was using as an engine

--- config/initializers/rspec_generator.rb
+++ config/initializers/rspec_generator.rb
@@ -1,4 +1,4 @@
-if Rails.env.test?
+if Rails.env.test? && defined?(Locomotive::Application) # does not need it for the engine version
   Locomotive::Application.configure do
     config.generators do |g|
       g.integration_tool :rspec
