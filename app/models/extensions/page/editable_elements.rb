9dd8783a66b7aecec649ac5653343e019296ba7b
Mario Visic <mario@mariovisic.com>
2011-02-15 22:53:29 +0800

    Altered the editable element inheritable checking to be more precise

--- app/models/extensions/page/editable_elements.rb
+++ app/models/extensions/page/editable_elements.rb
@@ -62,7 +62,7 @@ module Models
 
           def merge_editable_elements_from_page(source)
             source.editable_elements.each do |el|
-              next if el.disabled? || el.inheritable.blank?
+              next if el.disabled? || el.inheritable == false
 
               existing_el = self.find_editable_element(el.block, el.slug)
 
