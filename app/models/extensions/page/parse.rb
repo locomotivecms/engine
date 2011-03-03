c998d4150c1d252f8e2695e90a1eca3e59c7a8d2
Dirk Kelly <dk@dirkkelly.com>
2011-03-03 08:06:54 +0800

    Added fullpath and the error message to liquid syntax error. Translations are pretty dodgy (sorry)

--- app/models/extensions/page/parse.rb
+++ app/models/extensions/page/parse.rb
@@ -37,7 +37,7 @@ module Models
               begin
                 self._parse_and_serialize_template
               rescue ::Liquid::SyntaxError => error
-                @parsing_errors << :liquid_syntax
+                @parsing_errors << I18n.t(:liquid_syntax, :fullpath => self.fullpath, :error => error.to_s,:scope => [:errors, :messages])
               rescue ::Locomotive::Liquid::PageNotFound => error
                 @parsing_errors << I18n.t(:liquid_extend, :fullpath => self.fullpath,:scope => [:errors, :messages])
               end
