d2f5757d5b4dab3cb77ecc8abe8bec67f59702d0
did <didier@nocoffee.fr>
2011-03-06 23:36:30 +0100

    little trick to increase performance in dev mode when used as an engine + do some cleaning in the TODO file

--- lib/locomotive/engine.rb
+++ lib/locomotive/engine.rb
@@ -25,6 +25,8 @@ $:.unshift File.dirname(__FILE__)
 module Locomotive
   class Engine < Rails::Engine
 
+    config.autoload_once_paths += %W( #{config.root}/app/controllers #{config.root}/app/models #{config.root}/app/helpers #{config.root}/app/uploaders)
+
     rake_tasks do
       load "railties/tasks.rake"
     end
