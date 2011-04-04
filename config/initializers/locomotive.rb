60df5c19f9af9cb9139572d8c7ea16263c409841
dinedine <didier.lafforgue@gmail.com>
2011-04-04 02:03:23 +0200

    the locomotive config template is now up-to-date

--- config/initializers/locomotive.rb
+++ config/initializers/locomotive.rb
@@ -21,18 +21,18 @@ Locomotive.configure do |config|
   # configure the hosting target for the production environment. Locomotive can be installed in:
   # - your own server
   # - Heroku (you need to create an account in this case)
-  # - Bushi.do (you need to create an account in this case)
+  # - Bushi.do (see the bushi.do website for more explanations)
   #
   # the possible options are: server, heroku, bushido or auto (default)
   # if you select 'auto', Locomotive will look after specific ENV variables to check
-  # the matching platform (Heroku and Bushido set their own ENV variable).
+  # the matching platform (Heroku and Bushido set their own ENV variables).
   #
   config.hosting = :auto
 
   # In case you host Locomotive in Heroku, the engine uses the heroku api to add / remove domains.
   # there are 2 ways of passing heroku credentials to Locomotive
   #   - from ENV variables: HEROKU_LOGIN & HEROKU_PASSWORD
-  #   - from this file
+  #   - from this file, see the example below and uncomment it if needed
   # config.heroku = {
   #   :login      => '<your_heroku_login>'
   #   :password   => '<your_heroku_password>'
