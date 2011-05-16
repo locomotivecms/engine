04ea8fa2893c52710898db81e62de77daf35ba74
Mario Visic <mario@mariovisic.com>
2011-05-16 08:21:05 +0800

    Added dragonfly config to find the correct convert location.

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,6 +1,13 @@
 require 'dragonfly/rails/images'
 
 Dragonfly[:images].configure do |c|
+
+  # Convert absolute location needs to be specified
+  # to avoid issues with Phusion Passenger not using $PATH
+  convert = `which convert`.presence || "/usr/local/bin/convert"
+  c.convert_command  = convert
+  c.identify_command = convert
+
   c.allow_fetch_url  = true
   c.allow_fetch_file = true
 end
