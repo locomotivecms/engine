fa910b7a7f52f6822adc4196fe8dddd7985ff370
Mario Visic <mario@mariovisic.com>
2011-06-06 13:38:50 +0800

    The rendering controller now uses action controller metal, manually included required modules.

--- app/controllers/admin/rendering_controller.rb
+++ app/controllers/admin/rendering_controller.rb
@@ -1,8 +1,27 @@
 module Admin
-  class RenderingController < ActionController::Base
+  class RenderingController < ActionController::Metal
 
+    # Logging
+    include AbstractController::Logger
+
+    # Rendering and redirection support
+    include ActionController::Redirecting
+    include ActionController::Rendering
+
+    # Devise and other helper methods
+    include ActionController::Helpers
+    include Devise::Controllers::Helpers
+    include Rails.application.routes.url_helpers
+
+    # Flash and CSRF form support
+    include ActionController::Flash
+    include ActionController::RequestForgeryProtection
+
+    # Before filters
+    include AbstractController::Callbacks
+
+    # Locomotive Routing and Rendering
     include Locomotive::Routing::SiteDispatcher
-
     include Locomotive::Render
 
     before_filter :require_site
