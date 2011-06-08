575ed35d899c55a45d1db438908881f71fba35f3
Mario Visic <mario@mariovisic.com>
2011-06-08 22:21:12 +0800

    Fixes to have the admin 404 working correctly again.

--- app/controllers/admin/rendering_controller.rb
+++ app/controllers/admin/rendering_controller.rb
@@ -4,6 +4,7 @@ module Admin
     # Rendering and redirection support
     include ActionController::Redirecting
     include ActionController::Rendering
+    include AbstractController::AssetPaths
 
     # Devise and other helper methods
     include ActionController::Helpers
@@ -21,12 +22,25 @@ module Admin
     include Locomotive::Render
 
     before_filter :require_site
+    before_filter :setup_paths
 
     helper 'admin/base'
 
     def show
-    append_view_path Rails.root.join('app', 'views')
       render_locomotive_page
+    end
+
+    private
+
+    def setup_paths
+      append_view_path Rails.root.join('app', 'views')
+
+      self.asset_host = ''
+      self.asset_path = Rails.root.join('app', 'public')
+      self.assets_dir = 'assets'
+      self.javascripts_dir = 'javascripts'
+      self.stylesheets_dir = 'stylesheets'
+
     end
 
   end
