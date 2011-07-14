5f29ebd55b0b07019c2571b5ff8897e15c58fbaf
did <didier@nocoffee.fr>
2011-07-14 15:01:34 +0200

    when exporting a site, a nil value for the listed page property should be considered as false

--- lib/locomotive/export.rb
+++ lib/locomotive/export.rb
@@ -163,9 +163,11 @@ module Locomotive
     protected
 
     def _copy_pages(page)
-      attributes = self.extract_attributes(page, %w{title seo_title meta_description meta_keywords listed redirect_url content_type published})
+      attributes = self.extract_attributes(page, %w{title seo_title meta_description meta_keywords redirect_url content_type published})
 
-      unless page.redirect?
+      attributes['listed'] = page.listed? # in some cases, page.listed can be nil
+
+      unless page.raw_template.blank?
         attributes.delete('redirect_url')
 
         if page.templatized?
