c71999c592ef886e1bd7a07d886322e96706407d
Anders Norås <anders@snehvit.local>
2011-07-16 09:57:40 +0200

    Fixed some issues with models paths.

--- lib/locomotive/engine.rb
+++ lib/locomotive/engine.rb
@@ -36,12 +36,8 @@ module Locomotive
       load "railties/tasks.rake"
     end
 
-    def self.root
-      File.expand_path(File.dirname(File.dirname(__FILE__)))
-    end
-
     def self.models_dir
-      "#{root}/app/models"
+      "#{config.root}/app/models/"
     end
 
   end
