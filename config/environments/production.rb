694ca19324026143354177351dba98ec0351ecfd
Sean Grove <s@bushi.do>
2011-07-21 18:38:58 -0700

    Le sigh...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Locomotive::Application.configure do
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
