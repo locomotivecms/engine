2a9a49f1b16801fe0756184634bf58333c4809ca
Sean Grove <s@bushi.do>
2011-07-21 18:48:46 -0700

    No more need for debug output

--- lib/locomotive/hosting/bushido/devise.rb
+++ lib/locomotive/hosting/bushido/devise.rb
@@ -6,27 +6,20 @@ module Locomotive
         extend ActiveSupport::Concern
 
         included do
-          puts "Including special bushido admin methods"
           alias_method_chain :require_admin, :bushido
         end
 
         module InstanceMethods
 
           def require_admin_with_bushido
-            puts "requiring admin with bushido.."
-            puts "App claimed?"
             if ::Locomotive.bushido_app_claimed?
-              
               require_admin_without_bushido
             else
-              puts "not claimed! signing in #{Account.order(:created_at).first.inspect}"
               # open back-office for everybody as long as the application is not claimed
               sign_in(Account.order_by(:created_at).first)
             end
           end
-
         end
-
       end
     end
   end
