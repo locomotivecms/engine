abbc151ed2a08a7cd5204cad0d151c1c7a22f4bf
did <didier@nocoffee.fr>
2011-07-26 23:11:43 +0200

    new factory_girl gem + fix the require pb with the devise_bushido_authenticatable gem

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'rails', '3.0.9'
 
 gem 'warden'
 gem 'devise', '1.3.4'
-gem 'devise_bushido_authenticatable', '1.0.0.alpha10'
+gem 'devise_bushido_authenticatable', '1.0.0.alpha10', :require => 'devise_cas_authenticatable'
 
 gem 'mongoid', '~> 2.0.2'
 gem 'bson_ext', '~> 1.3.0'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'factory_girl'
 
-Factory.find_definitions
+FactoryGirl.find_definitions
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
