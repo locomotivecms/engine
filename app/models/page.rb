27d73c12237a3a73a02356c8649207a2b9408deb
Anders Norås <mail@andersnoras.com>
2011-08-03 02:36:17 +0200

    Excluded pages that require authentication from nav when no user is authenticated.

--- app/models/page.rb
+++ app/models/page.rb
@@ -46,7 +46,7 @@ class Page
   scope :not_found, :where => { :slug => '404', :depth => 0 }
   scope :published, :where => { :published => true }
   scope :fullpath, lambda { |fullpath| { :where => { :fullpath => fullpath } } }
-  scope :minimal_attributes, :only => %w(title slug fullpath position depth published templatized redirect listed parent_id created_at updated_at)
+  scope :minimal_attributes, :only => %w(title slug fullpath position depth published templatized redirect authentication_required listed parent_id created_at updated_at)
 
   ## methods ##
 
