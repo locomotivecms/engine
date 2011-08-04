1e24a01f761a837ba4143a5c0821bfbd89417100
Alex Sanford <alex.sanford1@gmail.com>
2011-08-04 11:03:30 -0300

    Set up specs to use spork for faster test runs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,52 +1,62 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require 'rubygems'
+require 'spork'
 
-require 'rspec/rails'
-require 'factory_girl'
+Spork.prefork do
+  # This file is copied to spec/ when you run 'rails generate rspec:install'
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path('../../config/environment', __FILE__)
 
-FactoryGirl.find_definitions
+  require 'rspec/rails'
+  require 'factory_girl'
+  Factory.find_definitions
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-Locomotive.configure_for_test
+  Locomotive.configure_for_test
 
-RSpec.configure do |config|
+  RSpec.configure do |config|
 
-  config.include(Locomotive::RSpec::Matchers)
+    config.include(Locomotive::RSpec::Matchers)
 
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  # config.mock_with :rspec
+    # == Mock Framework
+    #
+    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+    #
+    config.mock_with :mocha
+    # config.mock_with :flexmock
+    # config.mock_with :rr
+    # config.mock_with :rspec
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+    # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  # config.use_transactional_fixtures = true
+    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+    # examples within a transaction, remove the following line or assign false
+    # instead of true.
+    # config.use_transactional_fixtures = true
 
-  config.before(:each) do
-    Locomotive.config.heroku = false
-  end
+    config.before(:each) do
+      Locomotive.config.heroku = false
+    end
 
-  require 'database_cleaner'
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.orm = 'mongoid'
-  end
+    require 'database_cleaner'
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.orm = 'mongoid'
+    end
 
-  config.before(:each) do
-    if self.described_class != Locomotive::Import::Job
-      DatabaseCleaner.clean
+    config.before(:each) do
+      if self.described_class != Locomotive::Import::Job
+        DatabaseCleaner.clean
+      end
     end
   end
+end
+
+Spork.each_run do
+  Locomotive.configure_for_test(true)
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  Dir[Rails.root.join('app/models/*.rb')].each { |f| load f }
 end
