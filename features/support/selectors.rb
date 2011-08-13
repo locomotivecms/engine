48f869dc27e1dd19b842857a16a66de13b236d7a
M. Scott Ford <scott@corgibytes.com>
2011-08-12 22:56:48 -0400

    Added failing feature for to illustrate issue with has_many relationships.

--- features/support/selectors.rb
+++ features/support/selectors.rb
@@ -13,7 +13,13 @@ module HtmlSelectorsHelpers
 
     when "the main form"
       "form.formtastic"
-
+    
+    when "the list of added items"
+      ".has-many-selector li.item.added"
+    
+    when "the list of items"
+      ".has-many-selector"
+    
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
