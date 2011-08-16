e0b9367f10632b3b1cf39d4b5778f7132882c95d
did <didier@nocoffee.fr>
2011-08-16 20:58:23 +0200

    Got Mongoid and FactoryGirl to stop preloading models (#157)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,12 +35,17 @@ Spork.prefork do
 
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
+
+  # Avoid preloading models
+  require 'rails/mongoid'
+  Spork.trap_class_method(Rails::Mongoid, :load_models)
+
   require File.expand_path('../../config/environment', __FILE__)
 
   require 'rspec/rails'
-  require 'factory_girl'
 
-  FactoryGirl.find_definitions
+  require 'factory_girl'
+  Spork.trap_class_method(FactoryGirl, :find_definitions)
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
