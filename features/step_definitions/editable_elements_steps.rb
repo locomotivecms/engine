aa6eb1ea9db8096cf08f42ee2731834dadd31b8b
Mario Visic <mario@mariovisic.com>
2011-09-01 18:45:30 +0800

    Removed a duplicate step.

--- features/step_definitions/editable_elements_steps.rb
+++ features/step_definitions/editable_elements_steps.rb
@@ -1,7 +1,3 @@
-Given /^a simple page named "([^"]*)" with the body:$/ do |page_slug, page_contents|
-  @page = create_content_page(page_slug, page_contents)
-end
-
 # modify an editable element
 Given /^the editable element "([^"]*)" in the "([^"]*)" page with the content "([^"]*)"$/ do |slug, page_slug, content|
   page = @site.pages.where(:slug => page_slug).first
