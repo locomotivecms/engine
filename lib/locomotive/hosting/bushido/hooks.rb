43dee9391d6c266209925264d6b12edd1062773a
Sean Grove <s@bushi.do>
2011-11-16 11:21:01 -0800

    Uses a more careful approach when removing a user based on the bushido user.removed hook

--- lib/locomotive/hosting/bushido/hooks.rb
+++ lib/locomotive/hosting/bushido/hooks.rb
@@ -41,8 +41,11 @@ module Locomotive
               Locomotive.log "Devise username column: #{::Devise.cas_username_column}="
               Locomotive.log "Removing username: #{event['data'].try(:[], 'ido_id')}"
 
-              Account.exists?(::Devise.cas_username_column => event['data'].try(:[], 'ido_id')) and
-                Account.where(::Devise.cas_username_column => event['data'].try(:[], 'ido_id')).destroy
+              ido_id = event['data'].try(:[], 'ido_id') 
+
+              ido_id and
+                Account.exists?(::Devise.cas_username_column => ido_id) and
+                Account.where(::Devise.cas_username_column => ido_id).destroy
             end
 
           end
