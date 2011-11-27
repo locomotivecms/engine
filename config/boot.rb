05d9990e69bb7bda321012dc14a8090c690d42b9
Mario Visic <mario@mariovisic.com>
2011-11-27 20:05:39 +1100

    Use syck for yaml, fixes an issuew with new delayed job.

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,9 @@
 require 'rubygems'
+
+# Need to explicitly use syck for yaml
+require 'yaml'
+YAML::ENGINE.yamler = 'syck'
+
 # Set up gems listed in the Gemfile.
 if File.exist?(File.expand_path('../../Gemfile', __FILE__))
   require 'bundler'
