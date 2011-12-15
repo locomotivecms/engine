82a61b1d2dcff645b053f7e8d3dab0b82c78d23d
Robsteranium <robin@robsteranium.plus.com>
2011-12-15 03:16:11 +0000

    spec added to confirm content_type Date problem & patch.  Also 1.8.7 compatibility with syck init.

--- config/boot.rb
+++ config/boot.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 
 # Need to explicitly use syck for yaml
 require 'yaml'
-YAML::ENGINE.yamler = 'syck'
+YAML::ENGINE.yamler = 'syck' if defined?(YAML::ENGINE)
 
 # Set up gems listed in the Gemfile.
 if File.exist?(File.expand_path('../../Gemfile', __FILE__))
