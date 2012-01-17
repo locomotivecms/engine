7169abb8c21f612ca2258cfba3946caca895c326
did <didier@nocoffee.fr>
2012-01-17 03:21:30 +0100

    stylish buttons in the toolbar

--- app/middlewares/locomotive/inline_editor_middleware.rb
+++ app/middlewares/locomotive/inline_editor_middleware.rb
@@ -9,13 +9,10 @@ module Locomotive
       response = @app.call(env)
 
       unless response[1]['Editable'].blank?
-         Rails.logger.debug "==> #{ENV['PATH_INFO'].inspect}, warden ? #{env['warden'].inspect} /#{env['warden'].user} / #{env['warden'].authenticated?(:locomotive_account)}"
-
          html = response.last.body.to_s.gsub '</body>', %(
           <a href="_admin">Admin</a>
           </body>
          )
-
          [response[0], response[1], [html]]
       else
         response
