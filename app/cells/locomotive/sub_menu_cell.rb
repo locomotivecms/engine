a5e9e529961385a42975233c8c355e63556cf1dd
Didier Lafforgue <didier@nocoffee.fr>
2012-02-07 15:20:38 +0100

    allow the main app to add custom code in the locomotive layout

--- app/cells/locomotive/sub_menu_cell.rb
+++ app/cells/locomotive/sub_menu_cell.rb
@@ -5,7 +5,7 @@ module Locomotive
 
     def build_item(name, attributes)
       item = super
-      enhanced_class = "#{'on' if name.to_s == sections(:sub)} #{item[:class]}"
+      enhanced_class = "#{'on' if name.to_s == sections(:sub).to_s} #{item[:class]}"
       item.merge(:class => enhanced_class)
     end
 
