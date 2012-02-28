a1bc3db8a60e5610646b07200b0ac40cd499132a
did <didier.lafforgue@gmail.com>
2012-02-28 06:54:02 -0800

    path to codemirror/overlay.js has been modified in the last version of the codemirror gem

--- lib/generators/locomotive/install/templates/carrierwave.rb
+++ lib/generators/locomotive/install/templates/carrierwave.rb
@@ -9,11 +9,12 @@ CarrierWave.configure do |config|
     config.root = File.join(Rails.root, 'public')
 
   when :production
-    config.storage = :s3
-    config.s3_access_key_id = ENV['S3_KEY_ID']
+    # the following configuration works for Amazon S3
+    config.storage              = :s3
+    config.s3_access_key_id     = ENV['S3_KEY_ID']
     config.s3_secret_access_key = ENV['S3_SECRET_KEY']
-    config.s3_bucket = ENV['S3_BUCKET']
-    # config.s3_cname = 'ENV['S3_CNAME']
+    config.s3_bucket            = ENV['S3_BUCKET']
+    # config.s3_cname           = ENV['S3_CNAME'] # optional
 
   else
     # settings for the local filesystem
