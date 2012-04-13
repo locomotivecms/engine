edca6323fb03980ffaf857204dd1af661cc1a7b0
Mario Visic <mario@mariovisic.com>
2012-04-13 22:11:44 +0800

    Fixed a regression when updating javascript stringify. Fixes #355.

--- app/assets/javascripts/locomotive/utils/core_ext.js
+++ app/assets/javascripts/locomotive/utils/core_ext.js
@@ -18,7 +18,7 @@
 
   String.prototype.slugify = function(sep) {
     if (typeof sep == 'undefined') sep = '_';
-    var alphaNumRegexp = new RegExp('[^\w\\' + sep + ']', 'g');
+    var alphaNumRegexp = new RegExp('[^\\w\\' + sep + ']', 'g');
     var avoidDuplicateRegexp = new RegExp('[\\' + sep + ']{2,}', 'g');
     return this.replace(/\s/g, sep).replace(alphaNumRegexp, '').replace(avoidDuplicateRegexp, sep).toLowerCase()
   }
