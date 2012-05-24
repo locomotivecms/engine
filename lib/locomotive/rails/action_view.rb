662e359acbf268d881ed92b3cd0dca2ceb0cf819
Didier Lafforgue <didier@nocoffee.fr>
2012-05-24 19:19:46 +0200

    fix issue #393

---
+++ lib/locomotive/rails/action_view.rb
@@ -0,0 +1,9 @@
+# http://jfire.io/blog/2012/04/30/how-to-securely-bootstrap-json-in-a-rails-view/
+class ActionView::Base
+  def json_escape(s)
+    result = s.to_s.gsub('/', '\/')
+    s.html_safe? ? result.html_safe : result
+  end
+
+  alias j json_escape
+end
