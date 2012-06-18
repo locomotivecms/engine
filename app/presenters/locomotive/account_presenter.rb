b6311c671b02161e7b5ca0dcf47c419f8fa7e2ae
Charlie Greene <greeneca@gmail.com>
2012-06-18 12:20:38 -0300

    allow access to encrypted password and password salt

--- app/presenters/locomotive/account_presenter.rb
+++ app/presenters/locomotive/account_presenter.rb
@@ -1,10 +1,10 @@
 module Locomotive
   class AccountPresenter < BasePresenter
 
-    delegate :name, :email, :locale, :to => :source
+    delegate :name, :email, :locale, :encrypted_password, :password_salt, :to => :source
 
     def included_methods
-      super + %w(name email locale)
+      super + %w(name email locale encrypted_password password_salt)
     end
 
   end
