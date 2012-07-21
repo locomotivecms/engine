ee9fc175020d5454a554a0b96043157bd820f881
Didier Lafforgue <didier@nocoffee.fr>
2012-07-21 23:34:19 +0200

    fix issue #468 + clean translations + fix an UI issue with the toolbar / toggle button

--- lib/locomotive/custom_fields.rb
+++ lib/locomotive/custom_fields.rb
@@ -30,6 +30,18 @@ module CustomFields
           "sites/#{model.site_id}/content_#{model.class.model_name.demodulize.underscore}/#{model.id}/files"
         end
 
+        # In some situations, for instance, for the notification email when a content entry is created,
+        # we need to know the url of the file without breaking the upload process.
+        # Actually, the uploaded file will be written on the filesystem after the email is sent.
+        #
+        # @return [ String ] The url to the soon uploaded file
+        #
+        def guess_url
+          this = self.class.new(model, mounted_as)
+          this.retrieve_from_store!(model.read_uploader(mounted_as))
+          this.url.to_s
+        end
+
         def cache_dir
           "#{Rails.root}/tmp/uploads"
         end
