2061047f8ce7c4c8162030bda78d1585e4bb9dd3
Didier Lafforgue <didier@nocoffee.fr>
2012-07-27 00:45:36 +0200

    fix issue #474

--- lib/locomotive/middlewares/inline_editor.rb
+++ lib/locomotive/middlewares/inline_editor.rb
@@ -7,22 +7,27 @@ module Locomotive
       end
 
       def call(env)
-        response = @app.call(env)
+        status, headers, response = @app.call(env)
 
-        unless response[1]['Editable'].blank?
-           html = response.last.body.to_s.gsub '</body>', %(
-            <a  href="_admin"
-                onmouseout="this.style.backgroundPosition='0px 0px'"
-                onmouseover="this.style.backgroundPosition='0px -45px'"
-                onmousedown="this.style.backgroundPosition='0px -90px'"
-                onmouseup="this.style.backgroundPosition='0px 0px'"
-                style="display: block;z-index: 1031;position: fixed;top: 10px; right: 10px;width: 48px; height: 45px;text-indent:-9999px;text-decoration:none;background: transparent url\('/assets/locomotive/icons/start.png'\) no-repeat 0 0;">
-            Admin</a>
-            </body>
-           )
-           [response[0], response[1], [html]]
-        else
-          response
+        response = modify(response) unless headers['Editable'].blank?
+
+        [status, headers, response]
+      end
+
+      def modify(response)
+        [].tap do |parts|
+          response.each do |part|
+            parts << part.to_s.gsub('</body>', %(
+             <a  href="_admin"
+                 onmouseout="this.style.backgroundPosition='0px 0px'"
+                 onmouseover="this.style.backgroundPosition='0px -45px'"
+                 onmousedown="this.style.backgroundPosition='0px -90px'"
+                 onmouseup="this.style.backgroundPosition='0px 0px'"
+                 style="display: block;z-index: 1031;position: fixed;top: 10px; right: 10px;width: 48px; height: 45px;text-indent:-9999px;text-decoration:none;background: transparent url\('/assets/locomotive/icons/start.png'\) no-repeat 0 0;">
+             Admin</a>
+             </body>
+             ))
+          end
         end
       end
 
