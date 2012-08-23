e4b69459e32a85b43b2d7a04fa5dbb721f6ee2ce
did <didier.lafforgue@gmail.com>
2012-08-23 09:13:53 +0200

    robots.txt raised an error if empty

--- app/controllers/locomotive/public/robots_controller.rb
+++ app/controllers/locomotive/public/robots_controller.rb
@@ -7,8 +7,8 @@ module Locomotive
       respond_to :txt
 
       def show
-        template = ::Liquid::Template.parse(current_site.robots_txt)
-        render :text => template.render('request_host' => self.request.host.downcase)
+        robots_template = ::Liquid::Template.parse(current_site.robots_txt)
+        render :text => robots_template.render('request_host' => self.request.host.downcase)
       end
 
     end
