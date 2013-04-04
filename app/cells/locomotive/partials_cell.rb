fe3cea68e93bf30929d52dbcf4cf7a57993761ab
Rodrigo Alvarez <papipo@gmail.com>
2013-04-04 10:34:24 +0200

    Added partials cell to inject templates anywhere

---
+++ app/cells/locomotive/partials_cell.rb
@@ -0,0 +1,21 @@
+module Locomotive
+  class PartialsCell < Cell::Rails
+    cattr_accessor :templates
+    
+    def self.add_template(namespace, name)
+      self.templates ||= {}
+      self.templates[namespace] ||= []
+      self.templates[namespace] << name
+    end
+    
+    def display(namespace, locals = {})
+      return unless self.class.templates && self.class.templates[namespace].present?
+      
+      locals.each_pair do |k,v|
+        instance_variable_set("@#{k}", v)
+      end
+      
+      self.class.templates[namespace].map { |template| render view: template }.join.html_safe
+    end
+  end
+end
