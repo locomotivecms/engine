57bb6278c3bf4849925636c97d5a10e085d18c6a
Ben Hutton <benhutton@gmail.com>
2013-06-10 16:29:17 -0400

    Rely on external gem for mongoid session store, and test it!

--- spec/dummy/config/initializers/session_store.rb
+++ spec/dummy/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Dummy::Application.config.session_store :cookie_store, key: '_dummy_session', :domain => :all
+# Dummy::Application.config.session_store :mongoid_store, key: '_dummy_session', :domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
