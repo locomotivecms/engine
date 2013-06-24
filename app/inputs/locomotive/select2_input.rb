00ae370bbe13010b46bd8e58b5853ed8e8df658f
Ivan Youroff <ivan.youroff@gmail.com>
2013-06-24 18:39:56 +0400

    addition to 189f46b

--- app/inputs/locomotive/select2_input.rb
+++ app/inputs/locomotive/select2_input.rb
@@ -14,29 +14,5 @@ module Locomotive
       opts[:current_value] = options[:current_value]._label if options[:current_value]
       opts
     end
-    
-    # def javascript
-    #   <<-eos
-    #     <script type="text/javascript">
-    #       $("##{input_html_options[:id]}").select2(#{select2_options.to_json})
-    #     </script>
-    #   eos
-    # end
-    # 
-    # def select2_options
-    #   opts = {
-    #     allowClear: include_blank,
-    #     width: '150px'
-    #   }
-    #   opts[:placeholder] = options[:prompt] if prompt?
-    #   opts[:ajax] = options[:ajax] if ajax?
-    # 
-    #   opts
-    # end
-    # 
-    # def ajax?
-    #   !!options[:ajax]
-    # end
-    
   end
 end
