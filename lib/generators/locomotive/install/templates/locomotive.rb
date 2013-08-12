4efbb4ebd51890cc042a05c8bff4321d8fa3143b
did <didier.lafforgue@gmail.com>
2013-08-13 00:39:14 +0200

    do I have a obsessive-compulsive disorder ?

--- lib/generators/locomotive/install/templates/locomotive.rb
+++ lib/generators/locomotive/install/templates/locomotive.rb
@@ -23,7 +23,7 @@ Locomotive.configure do |config|
   #   per_page:           10
   # }
 
-  # default locale (for now, only en, de, fr, pl, pt-BR, it, nb, ja, zh-CN ,cs and bg are supported)
+  # default locale (for now, only en, de, fr, pl, pt-BR, it, nb, ja, zh-CN, cs and bg are supported)
   config.default_locale = :en
 
   # available locales suggested to "localize" a site. You will have to pick up at least one among that list.
