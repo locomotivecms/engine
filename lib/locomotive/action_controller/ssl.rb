9acc14ab685207de17b07ab24abef2eed3ecfabe
did <didier.lafforgue@gmail.com>
2013-08-16 10:36:05 +0200

    ssl only for the root domain

--- lib/locomotive/action_controller/ssl.rb
+++ lib/locomotive/action_controller/ssl.rb
@@ -2,12 +2,19 @@ module Locomotive
   module ActionController
     module Ssl
 
-      protected
-
       def require_ssl
-        redirect_to protocol: 'https://' if Locomotive.config.enable_admin_ssl && !request.ssl?
+        # already a ssl request or ssl disabled ?
+        return if !Locomotive.config.enable_admin_ssl || request.ssl?
+
+        # only require ssl for requests for the main domain
+        if !Locomotive.config.multi_sites? || Locomotive.config.multi_sites.domain == request.domain
+          redirect_to protocol: 'https://'
+        end
+
+        true
       end
 
     end
   end
 end
+
