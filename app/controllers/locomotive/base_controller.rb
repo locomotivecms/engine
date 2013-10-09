0105877aec40219e72b70dee5bbf8ef21be03c2c
did <didier.lafforgue@gmail.com>
2013-10-09 12:33:28 +0200

    locales are set BEFORE loading any model from the database

--- app/controllers/locomotive/base_controller.rb
+++ app/controllers/locomotive/base_controller.rb
@@ -16,15 +16,15 @@ module Locomotive
 
     before_filter :require_site
 
+    before_filter :set_back_office_locale
+
+    before_filter :set_current_content_locale
+
     before_filter :validate_site_membership
 
     load_and_authorize_resource
 
     around_filter :set_timezone
-
-    before_filter :set_back_office_locale
-
-    before_filter :set_current_content_locale
 
     before_filter :set_current_thread_variables
 
