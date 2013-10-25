2d2eb36a5142e09d8f571b774cf6438df5bb0278
did <didier.lafforgue@gmail.com>
2013-10-25 11:33:13 -0400

    fix issue #818

--- lib/locomotive/liquid/tags/nav.rb
+++ lib/locomotive/liquid/tags/nav.rb
@@ -82,7 +82,7 @@ module Locomotive
 
         # Returns a list element, a link to the page and its children
         def render_entry_link(context, page, css, depth)
-          selected = @page.fullpath =~ /^#{page.fullpath}/ ? " #{@options[:active_class]}" : ''
+          selected = @page.fullpath =~ /^#{page.fullpath}(\/.*)?$/ ? " #{@options[:active_class]}" : ''
 
           icon  = @options[:icon] ? '<span></span>' : ''
           title = render_title(context, page)
