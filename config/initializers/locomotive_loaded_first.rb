e4b7364e713408d9211c0ffc2b3952cc79c30a28
did <didier.lafforgue@gmail.com>
2014-04-19 17:41:15 +0200

    models from the engine were not loaded first in an app hosted in Heroku

--- config/initializers/locomotive_loaded_first.rb
+++ config/initializers/locomotive_loaded_first.rb
@@ -6,7 +6,7 @@ module ActiveSupport::Dependencies
 
   def require_or_load(file_name, const_path = nil)
     if file_name.starts_with?(Rails.root.to_s + '/app')
-      relative_name = file_name.gsub(Rails.root.to_s, '')
+      relative_name = file_name.gsub(/^#{Rails.root.to_s}/, '')
       @engine_paths ||= Rails::Application::Railties.engines.collect{|engine| engine.config.root.to_s }
       @engine_paths.each do |path|
         engine_file = File.join(path, relative_name)
