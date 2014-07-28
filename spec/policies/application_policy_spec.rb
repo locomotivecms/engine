2065d851d2d0c54107776dc5cd3a9c39e9347243
Joel AZEMAR <joel.azemar@gmail.com>
2014-07-28 12:57:41 +0200

    better naming

--- spec/policies/application_policy_spec.rb
+++ spec/policies/application_policy_spec.rb
@@ -1,27 +1,41 @@
 require 'spec_helper'
 
 module Locomotive
+
   describe ApplicationPolicy do
-    let(:record) { mock('record') }
+    let(:resource) { :site}
     let(:user)   { create('admin user') }
+    let(:site) { create :site }
 
     subject { ApplicationPolicy }
 
     context 'admin' do
+
+      module AccountMembershipWrapper
+        def membership()
+          Membership.new(account: self.user, role: 'admin')
+        end
+      end
+
       before do
         user.expects(:is_admin?).at_least_once.returns(true)
       end
 
       permissions *ApplicationPolicy::MANAGE_ACTIONS.map { |action| action.to_s + '?' } do
-        it('yes') { expect(subject).to permit(user, record) }
+        it('yes') { expect(subject).to permit(user, site, resource) }
       end
     end
 
     context 'anybody else' do
-      let(:user) { create(:account) }
+
+      module AccountMembershipWrapper
+        def membership()
+          Membership.new(account: self.user, role: 'guest')
+        end
+      end
 
       permissions *ApplicationPolicy::MANAGE_ACTIONS.map { |action| action.to_s + '?' } do
-        it('no') { expect(subject).to_not permit(user, record) }
+        it('no') { expect(subject).to_not permit(user, site, resource) }
       end
     end
   end
