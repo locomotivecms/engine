5df5cbf75b08f1841bb3c6739092fad1fce31bad
Joel AZEMAR <joel.azemar@gmail.com>
2014-07-29 15:37:47 +0200

    add policy to translations controller

--- app/controllers/locomotive/translations_controller.rb
+++ app/controllers/locomotive/translations_controller.rb
@@ -5,6 +5,8 @@ module Locomotive
 
     respond_to :json, only: [:create, :update]
 
+    before_filter :load_translation, only: [:edit, :update, :destroy]
+
     def index
       @translations = current_site.translations.ordered.page(params[:page]).per(Locomotive.config.ui[:per_page])
       respond_with @translations
@@ -16,6 +18,7 @@ module Locomotive
     end
 
     def create
+      authorize :translation
       @translation = current_site.translations.create(params[:translation])
       respond_with @translation, location: translations_path
     end
@@ -25,15 +28,21 @@ module Locomotive
     end
 
     def update
-      @translation = current_site.translations.find(params[:id])
+      authorize :translation
       @translation.update_attributes(params[:translation])
       respond_with @translation, location: translations_path
     end
 
     def destroy
-      @translation = current_site.translations.find(params[:id])
+      authorize :translation
       @translation.destroy
       respond_with @translation, location: translations_path
+    end
+
+    private
+
+    def load_translation
+      @translation = current_site.translations.find(params[:id])
     end
   end
 end
