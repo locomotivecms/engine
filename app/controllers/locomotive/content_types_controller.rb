ef196894b481a57e020867a2ff5f8fd5cff3a5a2
Joel AZEMAR <joel.azemar@gmail.com>
2014-07-29 09:35:53 +0200

    add policy to content types controller

--- app/controllers/locomotive/content_types_controller.rb
+++ app/controllers/locomotive/content_types_controller.rb
@@ -4,6 +4,7 @@ module Locomotive
     sections 'contents'
 
     before_filter :back_to_default_site_locale, only: %w(new create)
+    before_filter :load_content_type, only: [:edit, :update, :destroy]
 
     respond_to :json, only: [:create, :update, :destroy]
 
@@ -15,25 +16,31 @@ module Locomotive
     end
 
     def create
+      authorize :content_type
       @content_type = current_site.content_types.create(params[:content_type])
       respond_with @content_type, location: edit_content_type_path(@content_type._id)
     end
 
     def edit
-      @content_type = current_site.content_types.find(params[:id])
       respond_with @content_type
     end
 
     def update
-      @content_type = current_site.content_types.find(params[:id])
+      authorize :content_type
       @content_type.update_attributes(params[:content_type])
       respond_with @content_type, location: edit_content_type_path(@content_type._id)
     end
 
     def destroy
-      @content_type = current_site.content_types.find(params[:id])
+      authorize :content_type
       @content_type.destroy
       respond_with @content_type, location: pages_path
+    end
+
+    private
+
+    def load_content_type
+      @content_type = current_site.content_types.find(params[:id])
     end
 
   end
