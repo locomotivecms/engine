99d3163e643579fb789df7b51c8b24622dda4130
did <didier.lafforgue@gmail.com>
2015-01-14 01:29:52 +0100

    remove duplicate methods

--- app/helpers/locomotive/accounts_helper.rb
+++ app/helpers/locomotive/accounts_helper.rb
@@ -5,10 +5,5 @@ module Locomotive
       current_site.accounts.collect { |a| ["#{a.name} <#{a.email}>", a.id.to_s] }
     end
 
-    def account_avatar_and_name(account, size = '70x70#')
-      avatar  = image_tag(account_avatar_url(account, size), class: 'img-circle', style: 'width: 20px')
-      profile = avatar + account.name
-    end
-
   end
 end
