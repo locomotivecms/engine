620c1cedf1b9f4504c7640f62328f298226b4231
did <didier.lafforgue@gmail.com>
2015-05-27 16:02:03 +0200

    bump version to 3.0.0-alpha.2

--- lib/locomotive/version.rb
+++ lib/locomotive/version.rb
@@ -1,3 +1,6 @@
+# http://semver.org/
+# MAJOR.MINOR.PATCH format.
+# 1.0.0-alpha < 1.0.0-alpha.1 < 1.0.0-alpha.beta < 1.0.0-beta < 1.0.0-beta.2 < 1.0.0-beta.11 < 1.0.0-rc.1 < 1.0.0
 module Locomotive #:nodoc
-  VERSION = '3.0.0-alpha'
+  VERSION = '3.0.0-alpha.2'
 end
