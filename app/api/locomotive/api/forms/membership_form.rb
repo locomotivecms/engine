452b8d42a73b40b9ef38c0fd315f1bbe0bf7b8db
Nic Boie <boie0025@gmail.com>
2015-05-27 15:17:21 -0500

    Remove active model tracking for unpersisted attributes

--- app/api/locomotive/api/forms/membership_form.rb
+++ app/api/locomotive/api/forms/membership_form.rb
@@ -3,19 +3,19 @@ module Locomotive
     module Forms
 
       class MembershipForm < BaseForm
-        attrs :account_id, :role, :account_email
+        attrs :account_id, :role
 
         attr_accessor :site
+        attr_reader   :account_email
 
         def initialize(site, attributes = {})
           self.site = site
           super(attributes)
         end
 
-        # @note if account_email is set, use it to find the account.
         def account_email=(email)
           self.account_id = Account.find_by(email: email).try(:_id)
-          set_attribute(:account_email, email)
+          @account_email = email
         end
       end
 
