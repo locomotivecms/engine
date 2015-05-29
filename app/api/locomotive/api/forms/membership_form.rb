810a775392d28a56e17dca9d0bf575bac538b0f6
did <didier.lafforgue@gmail.com>
2015-05-30 00:30:49 +0200

    buttons to expand and close the drawer

--- app/api/locomotive/api/forms/membership_form.rb
+++ app/api/locomotive/api/forms/membership_form.rb
@@ -4,6 +4,19 @@ module Locomotive
 
       class MembershipForm < BaseForm
         attrs :account_id, :role
+
+        attr_accessor :site
+        attr_reader   :account_email
+
+        def initialize(site, attributes = {})
+          self.site = site
+          super(attributes)
+        end
+
+        def account_email=(email)
+          self.account_id = Account.find_by(email: email).try(:_id)
+          @account_email = email
+        end
       end
 
     end
--- app/assets/javascripts/locomotive/views/editable_elements/index_view.js.coffee
+++ app/assets/javascripts/locomotive/views/editable_elements/index_view.js.coffee
@@ -2,4 +2,10 @@ Locomotive.Views.EditableElements ||= {}
 
 class Locomotive.Views.EditableElements.IndexView extends Backbone.View
 
-  el: '#content'
+  el: '.content'
+
+  events:
+    'click .expand-button': 'expand_drawer'
+
+  expand_drawer: (event) ->
+    $('body').addClass('drawer-opened')
