89028711911081160549b4960efe0d8d078afbf4
did <didier.lafforgue@gmail.com>
2015-06-03 17:09:48 +0200

    fix and refactor the page parsing service (did not handle correctly fixed elements)

--- app/services/locomotive/page_parsing_service.rb
+++ app/services/locomotive/page_parsing_service.rb
@@ -5,9 +5,10 @@ module Locomotive
     def find_or_create_editable_elements(page)
       [].tap do |elements|
         ActiveSupport::Notifications.subscribe(/^steam\.parse\.editable\./) do |name, start, finish, id, payload|
-          current_page, attributes = payload[:page], payload[:attributes]
+          parsed_page, attributes = payload[:page], payload[:attributes]
 
-          _page = attributes[:fixed] ? current_page : page
+          # Note: parsed_page is a Steam entity
+          _page = attributes[:fixed] ? Locomotive::Page.find(parsed_page._id) : page
 
           elements << [_page, attributes]
         end
@@ -20,48 +21,37 @@ module Locomotive
 
     private
 
-    # def persist_editable_elements(page, elements)
-    def persist_editable_elements(elements)
-      elements.each do |couple|
-        _page, attributes = couple
-
-        # next unless can_persist_element?(page, current_page, attributes)
-
-        # update the element or or add the element to that page
-        if element = _page.editable_elements.by_block_and_slug(attributes[:block], attributes[:slug]).first
-          element.attributes = attributes
-        else
-          klass = "Locomotive::#{attributes[:type].to_s.classify}".constantize
-          element = _page.editable_elements.build(attributes, klass)
-        end
-
-        couple[1] = element
-
-        _page.save!
-
-        # binding.pry
-      end
-
-      # page.save!
-    end
-
-    # Fixed editable elements are not added to the parsed page unless
-    # they are defined in that page.
-    #
-    # @param [ Object ] page The page we are parsing
-    # @param [ Object ] current_page The page where the editable element were found
-    # @param [ Object ] attributes The attributes of the editable element
-    #
-    def can_persist_element?(page, current_page, attributes)
-      !attributes[:fixed] || (attributes[:fixed] && page._id == current_page._id)
-    end
-
     def parse(page)
       entity = repository.build(page.attributes.dup)
       decorated_page = Locomotive::Steam::Decorators::TemplateDecorator.new(entity, self.locale)
 
       parser = services.liquid_parser
       parser.parse(decorated_page)
+    end
+
+    def persist_editable_elements(elements)
+      modified_pages = [] # group modifications by page
+
+      elements.each do |couple|
+        _page, attributes = couple
+
+        element   = add_or_modify_editable_element(_page, attributes)
+        couple[1] = element
+
+        modified_pages << _page
+      end
+
+      modified_pages.uniq.map(&:save!)
+    end
+
+    def add_or_modify_editable_element(page, attributes)
+      if element = page.editable_elements.by_block_and_slug(attributes[:block], attributes[:slug]).first
+        element.attributes = attributes
+        element
+      else
+        klass = "Locomotive::#{attributes[:type].to_s.classify}".constantize
+        element = page.editable_elements.build(attributes, klass)
+      end
     end
 
     def services
