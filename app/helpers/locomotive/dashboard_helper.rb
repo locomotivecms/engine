ffd2725578b34fc2b074cb8d551024f16a0b6802
did <didier.lafforgue@gmail.com>
2015-07-24 23:36:40 +0200

    refactor the code of the activity feed display (mostly the helper)

--- app/helpers/locomotive/dashboard_helper.rb
+++ app/helpers/locomotive/dashboard_helper.rb
@@ -3,74 +3,73 @@ module Locomotive
 
     def activity_to_icon(activity)
       case activity.domain
-      when 'site' then 'fa-cog'
-      when 'page' then 'fa-file-text'
-      when 'content_entry' then activity.action == 'created_public' ? 'fa-comment' : 'fa-archive'
-      when 'membership' then 'fa-user'
-      when 'content_asset' then 'fa-file-picture-o'
+      when 'site'           then 'fa-cog'
+      when 'page'           then 'fa-file-text'
+      when 'content_entry'  then activity.action == 'created_public' ? 'fa-comment' : 'fa-archive'
+      when 'membership'     then 'fa-user'
+      when 'content_asset'  then 'fa-file-picture-o'
       end
     end
 
     def render_activity_sentence(activity)
       params = activity.parameters
 
-      case activity.key
-      when 'site.created'
-        I18n.t('locomotive.activity.site.created')
+      options = case activity.key
+      when 'membership.created' then { name: activity_emphasize(params[:name]) }
+      when /\Acontent_entry\./  then activity_content_entry_options(params)
+      when /\Apage\./           then activity_page_options(params)
+      when 'content_asset.created_bulk' then activity_bulk_content_assets_options(params)
+      when 'content_asset.destroyed' then { name: activity_emphasize(params[:name]) }
+      end
 
-      when 'membership.created'
-        I18n.t('locomotive.activity.membership.created', name: content_tag(:strong, params[:name]))
+      activity_key_to_sentence(activity.key, options)
+    end
 
-      when 'content_entry.created'
-        I18n.t 'locomotive.activity.content_entry.created',
-          entry: link_to(params[:label], edit_content_entry_path(current_site, params[:content_type_slug], params[:_id])),
-          content_type: link_to(params[:content_type], content_entries_path(current_site, params[:content_type_slug]))
-      when 'content_entry.created_public'
-        I18n.t 'locomotive.activity.content_entry.created_public',
-          entry: link_to(params[:label], edit_content_entry_path(current_site, params[:content_type_slug], params[:_id])),
-          content_type: link_to(params[:content_type], content_entries_path(current_site, params[:content_type_slug]))
-      when 'content_entry.updated'
-        I18n.t 'locomotive.activity.content_entry.updated',
-          entry: link_to(params[:label], edit_content_entry_path(current_site, params[:content_type_slug], params[:_id])),
-          content_type: link_to(params[:content_type], content_entries_path(current_site, params[:content_type_slug]))
-      when 'content_entry.destroyed'
-        I18n.t 'locomotive.activity.content_entry.destroyed',
-          entry: content_tag(:strong, params[:label]),
-          content_type: link_to(params[:content_type], content_entries_path(current_site, params[:content_type_slug]))
-      when 'content_entry.sorted'
-        I18n.t 'locomotive.activity.content_entry.sorted',
-          content_type: link_to(params[:content_type], content_entries_path(current_site, params[:content_type_slug]))
+    def activity_key_to_sentence(key, options = nil)
+      I18n.t(key, (options || {}).merge(scope: 'locomotive.activity')).html_safe
+    end
 
-      when 'page.created'
-        I18n.t 'locomotive.activity.page.created',
-          page: link_to(params[:title], editable_elements_path(current_site, params[:_id]))
-      when 'page.updated'
-        I18n.t 'locomotive.activity.page.updated',
-          page: link_to(params[:title], editable_elements_path(current_site, params[:_id]))
-      when 'page.destroyed'
-        I18n.t 'locomotive.activity.page.destroyed',
-          page: content_tag(:strong, params[:title])
-      when 'page.sorted'
-        I18n.t 'locomotive.activity.page.sorted'
+    def activity_emphasize(text)
+      content_tag(:strong, text)
+    end
 
-      when 'content_asset.created_bulk'
-        I18n.t('locomotive.activity.content_asset.created_bulk',
-          count: content_tag(:strong, params[:assets].size)) +
-        content_tag(:ul,
-          params[:assets].map do |asset|
-            if asset[:image]
-              content_tag(:li, link_to(image_tag(Locomotive::Dragonfly.resize_url(asset[:url], '60x60#'), alt: asset[:name]), asset[:url]))
-            else
-              content_tag(:li, link_to(truncate(asset[:name], length: 20), asset[:url]))
-            end
-          end.join("\n").html_safe, class: 'assets')
+    def activity_page_options(params)
+      if params[:_id]
+        { page: link_to(params[:title], editable_elements_path(current_site, params[:_id])) }
+      elsif params[:title]
+        { page: activity_emphasize(params[:title]) }
+      else
+        nil
+      end
+    end
 
-      when 'content_asset.destroyed'
-        I18n.t 'locomotive.activity.content_asset.destroyed',
-          filename: content_tag(:strong, params[:name])
+    def activity_content_entry_options(params)
+      entry = if params[:_id]
+        link_to(params[:label], edit_content_entry_path(current_site, params[:content_type_slug], params[:_id]))
+      elsif params[:label]
+        activity_emphasize(params[:label])
+      else
+        nil
+      end
 
-      else "Unknown activity #{activity.key}"
-      end.html_safe
+      content_type = link_to(params[:content_type], content_entries_path(current_site, params[:content_type_slug]))
+
+      { entry: entry, content_type: content_type }
+    end
+
+    def activity_bulk_content_assets_options(params)
+      list = params[:assets].map do |asset|
+        if asset[:image]
+          content_tag(:li, link_to(image_tag(Locomotive::Dragonfly.resize_url(asset[:url], '60x60#'), alt: asset[:name]), asset[:url]))
+        else
+          content_tag(:li, link_to(truncate(asset[:name], length: 20), asset[:url]))
+        end
+      end.join("\n").html_safe
+
+      {
+        count:  activity_emphasize(params[:assets].size),
+        list:   content_tag(:ul, list, class: 'assets')
+      }
     end
 
   end
