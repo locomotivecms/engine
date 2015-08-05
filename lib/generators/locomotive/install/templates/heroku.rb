343e90326b0eedb3240df448c287052afdf26796
did <didier.lafforgue@gmail.com>
2015-08-05 17:39:02 +0200

    apply back to the engine the code in charge of adding/deleting domains in Heroku

--- lib/generators/locomotive/install/templates/heroku.rb
+++ lib/generators/locomotive/install/templates/heroku.rb
@@ -1,6 +1,6 @@
 # Custom code if Locomotive is running on the Heroku platform
 
-if Rails.env.production? && api_key = ENV['HEROKU_API_KEY'] && app_name = ENV['HEROKU_APP_NAME']
+if Rails.env.production? && (api_key = ENV['HEROKU_API_KEY']) && (app_name = ENV['HEROKU_APP_NAME'])
   Rails.logger.info "[Locomotive] Heroku app detected"
 
   require 'platform-api'
@@ -9,11 +9,19 @@ if Rails.env.production? && api_key = ENV['HEROKU_API_KEY'] && app_name = ENV['H
 
   ActiveSupport::Notifications.subscribe('locomotive.site.domain_sync') do |name, start, finish, id, payload|
     (payload[:added] || []).each do |domain|
-      heroku.domain.create(app_name, hostname: domain)
+      begin
+        heroku.domain.create(app_name, hostname: domain)
+      rescue Exception => e
+        Rails.logger.error "Unable to add '#{domain}' as a new domain in Heroku, error #{e.message}"
+      end
     end
 
     (payload[:removed] || []).each do |domain|
-      heroku.domain.delete(app_name, domain)
+      begin
+        heroku.domain.delete(app_name, domain)
+      rescue Exception => e
+        Rails.logger.error "Unable to remove '#{domain}' from the list of domains in Heroku, error #{e.message}"
+      end
     end
   end
 end
