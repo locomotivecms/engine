927ee04dd7994f464eadd02fe8334aa396a6407c
did <didier.lafforgue@gmail.com>
2015-08-31 13:18:42 +0200

    the no_site_message_and_link helper method was undefined if the no_site exception was raised from a controller

--- app/helpers/locomotive/installation_helper.rb
+++
@@ -1,9 +0,0 @@
-module Locomotive
-  module InstallationHelper
-
-    def next_installation_step_link(step = 1, label = nil)
-      link_to(label || t('admin.installation.common.next'), installation_step_path(step), class: 'btn btn-block btn-danger')
-    end
-
-  end
-end
