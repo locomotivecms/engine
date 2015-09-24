57203ef8838e73adf0133071e97b0189e237c136
did <didier.lafforgue@gmail.com>
2015-09-23 23:24:28 -0500

    [travis] do not need to set a hostname for our specs

--- bin/ci/before_build.sh
+++ bin/ci/before_build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 sh -e /etc/init.d/xvfb start
-echo "echo \"127.0.0.1    test.example.com\" >> /etc/hosts" | sudo sh
-sudo hostname test.example.com
-echo 'America/Chicago' | sudo tee /etc/timezone
-sudo dpkg-reconfigure --frontend noninteractive tzdata
+export TZ=America/Chicago
 date
