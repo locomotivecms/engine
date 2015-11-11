88793df11806817316e2ed5d83148749b32aecb5
did <didier.lafforgue@gmail.com>
2015-11-11 02:39:33 +0100

    allow passing a picture as a hash for the API site resource

--- app/api/locomotive/api/resources/site_resource.rb
+++ app/api/locomotive/api/resources/site_resource.rb
@@ -71,7 +71,7 @@ module Locomotive
           post do
             authorize Site, :create?
 
-            site_form = Forms::SiteForm.new(permitted_params_from_policy(Locomotive::Site, :site))
+            site_form = Forms::SiteForm.new(permitted_params_from_policy(Locomotive::Site, :site, [:picture]))
             site = service.create!(site_form.serializable_hash)
 
             present site, with: entity_klass
@@ -98,7 +98,7 @@ module Locomotive
             site = load_site
             authorize site, :update?
 
-            site_form = Forms::SiteForm.new(permitted_params_from_policy(site, :site))
+            site_form = Forms::SiteForm.new(permitted_params_from_policy(site, :site, [:picture]))
             site.assign_attributes(site_form.serializable_hash)
             site.save!
 
