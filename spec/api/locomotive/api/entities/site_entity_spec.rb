7b2756c969ff93b81de8b9bde5281b02b7ef7940
did <didier.lafforgue@gmail.com>
2015-11-11 21:02:22 +0100

    the API site entity returns the preview url and a the sign in url

--- spec/api/locomotive/api/entities/site_entity_spec.rb
+++ spec/api/locomotive/api/entities/site_entity_spec.rb
@@ -10,10 +10,11 @@ describe Locomotive::API::Entities::SiteEntity do
     end
 
   context 'overrides' do
-    let!(:admin) { create(:admin) }
-    let!(:site) { admin.site }
+    let!(:admin)  { create(:admin) }
+    let!(:site)   { admin.site }
+    let(:options) { { env: { 'HTTP_HOST' => 'http://locomotive.works:8080' } } }
 
-    subject { described_class.new(site) }
+    subject { described_class.new(site, options) }
     let(:exposure) { subject.serializable_hash }
 
     describe 'content_version' do
@@ -37,6 +38,18 @@ describe Locomotive::API::Entities::SiteEntity do
     describe 'memberships' do
       it 'returns an array of memberships' do
         expect(exposure[:memberships]).to be_kind_of(Array)
+      end
+    end
+
+    describe 'preview_url' do
+      it 'returns the url to preview the site' do
+        expect(exposure[:preview_url]).to eq 'http://locomotive.works:8080/locomotive/acme/preview'
+      end
+    end
+
+    describe 'sign_in_url' do
+      it 'returns the url to sign in to the site' do
+        expect(exposure[:sign_in_url]).to eq 'http://locomotive.works:8080/locomotive/sign_in'
       end
     end
   end
