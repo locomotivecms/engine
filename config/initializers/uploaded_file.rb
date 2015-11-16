96523def8b5444f170e30029da394ae21297c70d
Dane Sowers <dsowers@kinetic-vision.com>
2015-11-16 11:32:42 -0500

    Updated ActionDispatch::Http::UploadedFile to_liquid method to not override tempfile attribute

--- config/initializers/uploaded_file.rb
+++ config/initializers/uploaded_file.rb
@@ -2,7 +2,7 @@ module ActionDispatch
 	module Http
 		class UploadedFile
 			def to_liquid
-				self.instance_values.merge('tempfile' => self.tempfile.path)
+				self.instance_values
 			end
 		end
 	end
