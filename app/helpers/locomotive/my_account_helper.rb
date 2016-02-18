e4f4eeb53b35fa669e95feb461800889268535a0
did <didier.lafforgue@gmail.com>
2016-02-19 00:40:05 +0100

    the options_for_locale form helper uses the Locomotive version for its cache key

--- app/helpers/locomotive/my_account_helper.rb
+++ app/helpers/locomotive/my_account_helper.rb
@@ -2,7 +2,7 @@ module Locomotive
   module MyAccountHelper
 
     def options_for_locale
-      Rails.cache.fetch('views/helpers/options_for_locale') do
+      Rails.cache.fetch("#{Locomotive::VERSION}/views/helpers/options_for_locale") do
         Locomotive.config.locales.map do |locale|
           flag_url = path_to_image "locomotive/icons/flags/#{locale}.png"
 
