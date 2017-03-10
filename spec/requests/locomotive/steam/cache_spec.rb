6a24430da0dfcc4bf0a391576228a80b2640a108
did <didier.lafforgue@gmail.com>
2017-03-10 16:11:16 +0100

    bump version to 3.2.1

--- spec/requests/locomotive/steam/cache_spec.rb
+++ spec/requests/locomotive/steam/cache_spec.rb
@@ -39,7 +39,7 @@ describe Locomotive::Steam::Middlewares::Cache do
 
     subject { middleware.send(:cache_key, steam_env) }
 
-    it { expect(subject).to eq 'e5bc074a017ba05e140a16185e3e38f7' }
+    it { expect(subject).to eq 'cb8743e499136fd491c386a25083aa6d' }
 
   end
 
