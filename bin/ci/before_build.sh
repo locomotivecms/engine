decd278694a9a0d9a5403dee407aa0c5ffb73e16
Didier Lafforgue <didier.lafforgue@gmail.com>
2018-04-04 18:42:46 +0200

    another attempt to install Google Chrome on Travis

--- bin/ci/before_build.sh
+++ bin/ci/before_build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-wget http://chromedriver.storage.googleapis.com/2.9/chromedriver_linux64.zip
+wget http://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 rm chromedriver_linux64.zip
-sudo apt-get install libnss3
-sudo apt-get --only-upgrade install google-chrome-stable
+# sudo apt-get install libnss3
+# sudo apt-get --only-upgrade install google-chrome-stable
 sudo cp chromedriver /usr/local/bin/.
 sudo chmod +x /usr/local/bin/chromedriver
+google-chrome-stable --headless --no-sandbox
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 export TZ=America/Chicago
@@ -13,4 +14,3 @@ sleep 3
 date
 whereis google-chrome-stable
 whereis chromedriver
-google-chrome-stable --headless --no-sandbox
