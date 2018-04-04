e1bf143fec91d4c58416d2a118109e161707adb3
Didier Lafforgue <didier.lafforgue@gmail.com>
2018-04-04 16:27:44 +0200

    fix the nprogress component

--- bin/ci/before_build.sh
+++ bin/ci/before_build.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-# wget http://chromedriver.storage.googleapis.com/2.34/chromedriver_linux64.zip
-# unzip chromedriver_linux64.zip
+wget http://chromedriver.storage.googleapis.com/2.9/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+rm chromedriver_linux64.zip
 # sudo apt-get install libnss3
 # sudo apt-get --only-upgrade install google-chrome-stable
-# sudo cp chromedriver /usr/local/bin/.
-# sudo chmod +x /usr/local/bin/chromedriver
-# export DISPLAY=:99.0
-# sh -e /etc/init.d/xvfb start
+sudo cp chromedriver /usr/local/bin/.
+sudo chmod +x /usr/local/bin/chromedriver
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
 export TZ=America/Chicago
 sleep 3
 date
 whereis google-chrome-stable
 whereis chromedriver
+google-chrome-stable --headless --no-sandbox
