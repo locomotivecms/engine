3c64c83ae5f7c4e511ae82ed57881687667b1c05
Didier Lafforgue <didier.lafforgue@gmail.com>
2018-04-04 15:34:32 +0200

    try to make Travis-CI work (WIP)

--- bin/ci/before_build.sh
+++ bin/ci/before_build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-wget http://chromedriver.storage.googleapis.com/2.34/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-sudo apt-get install libnss3
-sudo apt-get --only-upgrade install google-chrome-stable
-sudo cp chromedriver /usr/local/bin/.
-sudo chmod +x /usr/local/bin/chromedriver
-export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
+# wget http://chromedriver.storage.googleapis.com/2.34/chromedriver_linux64.zip
+# unzip chromedriver_linux64.zip
+# sudo apt-get install libnss3
+# sudo apt-get --only-upgrade install google-chrome-stable
+# sudo cp chromedriver /usr/local/bin/.
+# sudo chmod +x /usr/local/bin/chromedriver
+# export DISPLAY=:99.0
+# sh -e /etc/init.d/xvfb start
 export TZ=America/Chicago
 sleep 3
 date
