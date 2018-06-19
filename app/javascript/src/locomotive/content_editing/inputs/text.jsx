413879e71cd4a2dfa16f887ccd39849d0fe410db
Julien Girard <julien09girard@gmail.com>
2018-06-19 17:34:34 +0200

    add draftjs wysiwyg

--- app/javascript/src/locomotive/content_editing/inputs/text.jsx
+++ app/javascript/src/locomotive/content_editing/inputs/text.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { EditorState, convertToRaw, ContentState, SelectionState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
+
 
 class TextInput extends Component {
 
@@ -8,13 +13,54 @@ class TextInput extends Component {
     var value = props.data.settings[props.setting.id];
     if (value === undefined) value = props.setting.default || '';
 
-    this.state = { value };
+    //create new content with raw html
+    const { contentBlocks, entityMap } = htmlToDraft(value);
+    const contentState = ContentState.createFromBlockArray(contentBlocks, entityMap);
+    let editorState = EditorState.createWithContent(contentState);
 
-    this.handleChange = this.handleChange.bind(this);
+    var html = props.setting.html || false;
+
+    const line_height =  20;
+    let line_break = props.setting.line_break || false;
+
+    let editorHeight = (props.setting.rows || 5) * line_height;
+
+    this.state = {
+      editorState,
+      value,
+      html,
+      editorHeight,
+      line_break
+    };
+
+    this.inputOnChangeSanitizer = this.inputOnChangeSanitizer.bind(this);
+    this.editorOnChangeSanitizer = this.editorOnChangeSanitizer.bind(this);
   }
 
-  handleChange(event) {
-    const { value } = event.target;
+  formatLineBreak(text) {
+    return text
+      .replace(/<\/p>\n<p>/g, '<br>')
+      .replace(/<p>/g, '')
+      .replace(/<\/p>/g, '');
+  }
+
+  inputOnChangeSanitizer(event){
+    if(event.target) {
+      return event.target.value;
+    }
+    this.updateSectionValue(value);
+  }
+
+  editorOnChangeSanitizer(editorState) {
+    this.setState({editorState: editorState});
+    let value = draftToHtml(convertToRaw(editorState.getCurrentContent()));
+
+    if(this.state.line_break) value = this.formatLineBreak(value)
+
+    this.updateSectionValue(value);
+  };
+
+  updateSectionValue(value) {
     this.setState({ value }, () => {
       this.props.onChange(this.props.setting.type, this.props.setting.id, value)
     });
@@ -22,16 +68,31 @@ class TextInput extends Component {
 
   render() {
     const { setting } = this.props;
-
-    return (
-      <div className="editor-input editor-input-text">
-        <label>{setting.label}</label>
-        <br/>
-        <input type="text" value={this.state.value} onChange={this.handleChange} />
-      </div>
-    )
+    const { editorState } = this.state;
+    if(this.state.html) {
+      return (
+        <div>
+          <label>{setting.label}</label>
+          <div style={{"height": `${this.state.editorHeight}px`, "overflow": "scroll"}} >
+            <Editor
+              editorState={editorState}
+              wrapperClassName="demo-wrapper"
+              editorClassName="demo-editor"
+              onEditorStateChange={this.editorOnChangeSanitizer}
+            />
+          </div>
+        </div>
+      )
+    }else{
+      return (
+        <div className="editor-input editor-input-text">
+          <label>{setting.label}</label>
+          <br/>
+          <input type="text" value={this.state.value} onChange={this.inputOnChangeSanitizer} />
+        </div>
+      )
+    }
   }
-
 }
 
 export default TextInput;
