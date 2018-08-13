9ec7cba7616870c2850da72558c868f1a569a4fb
Julien Girard <julien09girard@gmail.com>
2018-08-13 18:54:05 +0200

    delete unused file

--- app/services/locomotive/section_service.rb
+++
@@ -1,8 +0,0 @@
-module Locomotive
-  class SectionService < struct.new(:site, :account)
-
-    def self.load_definition_json(section)
-      Locomotive::Steam::SectionFinderService.new(find section
-    end
-  end
-end
