6f0387b665b8e8946ddc0e602ff208e09c4076f1
Julien Girard <julien09girard@gmail.com>
2018-10-04 16:40:14 +0200

    fix

--- app/javascript/src/locomotive/editor/inputs/url.js
+++ app/javascript/src/locomotive/editor/inputs/url.js
@@ -6,7 +6,7 @@ import UrlPicker from '../components/url_picker';
 const UrlInput = props => (
   <div className="editor-input editor-input-url">
     <label className="editor-input--label">
-      {props.getValue(null)}
+      {props.getLabel()}
     </label>
     <UrlPicker
       value={props.getValue(null)}
