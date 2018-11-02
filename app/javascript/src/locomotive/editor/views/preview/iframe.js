4d833b84275ff59f47e4b1762f1d9f33cfa4c3b1
Didier Lafforgue <didier.lafforgue@gmail.com>
2018-11-02 17:32:37 +0100

    avoid double iframe rendering when dealing with sections

---
+++ app/javascript/src/locomotive/editor/views/preview/iframe.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { waitUntil, getMetaContentFromIframe } from '../../utils/misc';
+import classnames from 'classnames';
+
+// Services
+import { prepareIframe } from '../../services/preview_service';
+
+class Iframe extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.createdAt = new Date().getMilliseconds();
+  }
+
+  componentDidMount() {
+    this.iframe.onload = () => {
+      // bring some modifications to the iframe
+      prepareIframe(this.iframe.contentWindow);
+
+      // don't allow to go to another page if the changes have not been saved
+      this.iframe.contentWindow.onbeforeunload = () => {
+        return this.props.changed ? 'Changes unsaved!' : null;
+      }
+
+      this.iframe.contentWindow.onunload =() => {
+        this.createdAt = new Date().getMilliseconds();
+        this.props.startLoadingIframe(this.iframe.contentWindow);
+      }
+
+      if (this.props.iframeState.loaded === null) {
+        // alright, we are all good to display the first screen
+        waitUntil(this.createdAt, null, () => this.props.onIframeLoaded(this.iframe.contentWindow));
+      } else {
+        waitUntil(this.createdAt, null, () => {
+          // the user clicks on a link in the iframe.
+          this.props.reloadEditor(
+            getMetaContentFromIframe(this.iframe, 'locomotive-page-id'),
+            getMetaContentFromIframe(this.iframe, 'locomotive-content-entry-id'),
+            getMetaContentFromIframe(this.iframe, 'locomotive-locale')
+          )
+        });
+      }
+    }
+  }
+
+  // If no locale is included in the path (ex.: /index), Steam will use the
+  // locale sent by the browser. We don't want this behavior when previewing the site
+  // in the back-office, so we have to force the locale.
+  getPreviewPath() {
+    const { previewPath, currentLocale, defaultLocale } = this.props;
+    return currentLocale === defaultLocale ? `${previewPath}?locale=${locale}` : previewPath;
+  }
+
+  shouldComponentUpdate() {
+    // guarantees that the iframe will be not reloaded.
+    return false;
+  }
+
+  render() {
+    return (
+      <div className="scrollable">
+        <div className="embed-responsive embed-page">
+          <iframe
+            className="embed-responsive-item"
+            src={this.getPreviewPath()}
+            ref={el => this.iframe = el}>
+          </iframe>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default Iframe;
--- app/javascript/src/locomotive/editor/views/preview/index.js
+++ app/javascript/src/locomotive/editor/views/preview/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { waitUntil, getMetaContentFromIframe } from '../../utils/misc';
 import { bindAll } from 'lodash';
 import classnames from 'classnames';
 
@@ -8,60 +7,18 @@ import withRedux from '../../hoc/with_redux';
 
 // Components
 import ActionBar from './action_bar';
-
-// Services
-import { prepareIframe } from '../../services/preview_service';
+import Iframe from './iframe';
 
 class Preview extends React.Component {
 
   constructor(props) {
     super(props);
-    this.createdAt = new Date().getMilliseconds();
     bindAll(this, 'changeScreensize');
     this.state = { screensize: 'desktop' };
   }
 
-  componentDidMount() {
-    this.iframe.onload = () => {
-      // bring some modifications to the iframe
-      prepareIframe(this.iframe.contentWindow);
-
-      // don't allow to go to another page if the changes have not been saved
-      this.iframe.contentWindow.onbeforeunload = () => {
-        return this.props.changed ? 'Changes unsaved!' : null;
-      }
-
-      this.iframe.contentWindow.onunload =() => {
-        this.createdAt = new Date().getMilliseconds();
-        this.props.startLoadingIframe(this.iframe.contentWindow);
-      }
-
-      if (this.props.iframeState.loaded === null) {
-        // alright, we are all good to display the first screen
-        waitUntil(this.createdAt, null, () => this.props.onIframeLoaded(this.iframe.contentWindow));
-      } else {
-        waitUntil(this.createdAt, null, () => {
-          // the user clicks on a link in the iframe.
-          this.props.reloadEditor(
-            getMetaContentFromIframe(this.iframe, 'locomotive-page-id'),
-            getMetaContentFromIframe(this.iframe, 'locomotive-content-entry-id'),
-            getMetaContentFromIframe(this.iframe, 'locomotive-locale')
-          )
-        });
-      }
-    }
-  }
-
   changeScreensize(screensize) {
     this.setState({ screensize });
-  }
-
-  // If no locale is included in the path (ex.: /index), Steam will use the
-  // locale sent by the browser. We don't want this behavior when previewing the site
-  // in the back-office, so we have to force the locale.
-  getPreviewPath() {
-    const { previewPath, currentLocale, defaultLocale } = this.props;
-    return currentLocale === defaultLocale ? `${previewPath}?locale=${locale}` : previewPath;
   }
 
   render() {
@@ -75,15 +32,7 @@ class Preview extends React.Component {
           />
 
           <div className={classnames('preview-iframe', this.props.iframeState.loaded !== true ? 'preview-iframe--loading' : null)}>
-            <div className="scrollable">
-              <div className="embed-responsive embed-page">
-                <iframe
-                  className="embed-responsive-item"
-                  src={this.getPreviewPath()}
-                  ref={el => this.iframe = el}>
-                </iframe>
-              </div>
-            </div>
+            <Iframe {...this.props} />
 
             {!this.props.iframeState.loaded && (
               <div className="preview-iframe-loader">
