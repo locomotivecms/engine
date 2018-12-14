b518d6fdc5568b444db972d9105f5bbd235b972e
Guillaume Noireaux <gnoireaux@gmail.com>
2018-12-14 04:06:35 +0100

    Update before_build.sh
    Unbreak the integration tests by adjusting google chrome version with its driver version.

--- bin/ci/before_build.sh
+++ bin/ci/before_build.sh
@@ -6,11 +6,19 @@ rm chromedriver_linux64.zip
 # sudo apt-get --only-upgrade install google-chrome-stable
 sudo cp chromedriver /usr/local/bin/.
 sudo chmod +x /usr/local/bin/chromedriver
+whereis chromedriver
+chromedriver --version
+
+whereis google-chrome-stable
+google-chrome-stable --version
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+whereis google-chrome-stable
+google-chrome-stable --version
+
 google-chrome-stable --headless --no-sandbox
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 export TZ=America/Chicago
 sleep 3
 date
-whereis google-chrome-stable
-whereis chromedriver
