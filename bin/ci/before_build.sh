5d32acbfba653f12a8921e0964d1a66d68155063
Didier Lafforgue <didier.lafforgue@gmail.com>
2019-01-09 00:10:12 +0100

    change the settings of Xvfb for Travis-ci

--- bin/ci/before_build.sh
+++ bin/ci/before_build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # wget http://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip
 # unzip chromedriver_linux64.zip
 # rm chromedriver_linux64.zip
@@ -9,6 +10,11 @@
 # sudo chmod +x /usr/local/bin/chromedriver
 # google-chrome-stable --headless --no-sandbox
 
+# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+# sudo apt-get update && sudo apt-get install yarn
+
+sleep 3
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 export TZ=America/Chicago
