a76f59ee9989fe14ff52aff7e19e64d39700825d
Didier Lafforgue <didier.lafforgue@gmail.com>
2019-04-11 22:53:10 +0200

    implement feature #1276

--- app/javascript/src/locomotive/editor/inputs/hint.js
+++ app/javascript/src/locomotive/editor/inputs/hint.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const HintInput = ({ label }) => (
   <div className="editor-input editor-input-hint">
-    <label className="editor-input--label">
-      {label}
-    </label>
+    <label className="editor-input--label" dangerouslySetInnerHTML={{ __html: label }} />
   </div>
 )
 
