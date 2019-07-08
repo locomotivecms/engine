55bdcabad93ef63971540043ba8912e7392269a4
Didier Lafforgue <didier.lafforgue@gmail.com>
2019-07-08 23:08:10 +0200

    use valid images for the specs about the sections

--- spec/support/features/site_helpers.rb
+++ spec/support/features/site_helpers.rb
@@ -37,9 +37,9 @@ module Features
         sections_content: {
           header: { settings: { title: 'This is the header' } },
           gallery: { settings: {}, blocks: [
-            { type: 'image', settings: { image: '/images/image_01.png' } },
-            { type: 'image', settings: { image: '/images/image_02.png' } },
-            { type: 'image', settings: { image: '/images/image_03.png' } }
+            { type: 'image', settings: { image: 'https://via.placeholder.com/150' } },
+            { type: 'image', settings: { image: 'https://via.placeholder.com/300' } },
+            { type: 'image', settings: { image: 'https://via.placeholder.com/300' } }
           ] }
         }
       )
