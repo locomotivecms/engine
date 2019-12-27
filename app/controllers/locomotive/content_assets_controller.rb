7fec5b571f3a5a22d7fca84906e0606393a2b213
Leonardo Diez <leiodd@gmail.com>
2019-12-27 13:43:15 +0100

    Fix bug on assets bulk creation
    `respond_with` method allow to respond with several resources as different parameters, not as an array of resources in one parameter.

--- app/controllers/locomotive/content_assets_controller.rb
+++ app/controllers/locomotive/content_assets_controller.rb
@@ -24,7 +24,7 @@ module Locomotive
     def bulk_create
       authorize Locomotive::ContentAsset, :create?
       @content_assets = service.bulk_create(content_assets_params)
-      respond_with @content_assets, location: content_assets_path
+      respond_with *@content_assets, location: content_assets_path
     end
 
     def edit
