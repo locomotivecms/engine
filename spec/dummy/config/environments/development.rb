97b9b6d61d65e263a0332c52067555097d802485
Didier Lafforgue <didier.lafforgue@gmail.com>
2023-03-02 19:07:41 +0100

    allow the example.com host in order for Wagon to pass its specs

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -58,4 +58,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << 'www.example.com'
 end
