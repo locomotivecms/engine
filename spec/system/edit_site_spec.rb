05a3cf8712a02534c3bc7d8e07e3479065c5283f
Daniel Hogenelst <daniel@winkwaves.com>
2024-03-29 22:54:38 +0100

    A spec for exposing a locale-related bug

--- spec/system/edit_site_spec.rb
+++ spec/system/edit_site_spec.rb
@@ -10,5 +10,10 @@ describe 'Editing a site' do
     click_button 'Save'
     expect(page).to have_content('My site was successfully updated.')
   end
-
+ 
+  it 'does not leak locales into input\'s value' do
+    click_button 'Save'
+    click_link 'SEO'
+    expect(page).not_to have_field('site_seo_title', with: 'en ')
+  end
 end
